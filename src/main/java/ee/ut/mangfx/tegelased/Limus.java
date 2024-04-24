be6cb1e0816c5705ae612ded8e222ebdd01907ba
a-m-muru <aksel.martin.muru@ut.ee>
2024-04-24 14:02:39 +0300

    limused

---
+++ src/main/java/ee/ut/mangfx/tegelased/Limus.java
@@ -0,0 +1,30 @@
+package ee.ut.mangfx.tegelased;
+
+import ee.ut.mangfx.abi.Koordinaat;
+import ee.ut.mangfx.abi.Sonumid;
+import ee.ut.mangfx.maailm.Maailm;
+
+public class Limus extends Tegelane {
+    public Limus(Maailm maailm, int xPos, int yPos) {
+        super(maailm, xPos, yPos);
+        seaSymbol('o');
+    }
+
+    @Override
+    public void teeMidagi() {
+        int kuhu = (int) (Math.random() * 3) - 1;
+        boolean xVoiY = Math.random() < 0.5;
+        Koordinaat suund = new Koordinaat(xVoiY ? kuhu : 0, !xVoiY ? kuhu : 0);
+        muudaPos(suund);
+    }
+
+    @Override
+    protected boolean kasVoibSinnaMinna(Koordinaat kuhu) {
+        Tegelane tegelane = maailm.hangiTegelane(kuhu);
+        if (tegelane != null && tegelane.getClass() != this.getClass()) {
+            tegelane.muudaElusid(-1);
+            Sonumid.lisaSonum("Limus ründas tegelast " + tegelane.hangiSymbol());
+        }
+        return super.kasVoibSinnaMinna(kuhu);
+    }
+}
